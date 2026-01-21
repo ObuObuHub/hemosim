b0610fc69f855765977f7a662304d5aec4324893
Andrei Chiper <andreichiper@MacBook-Air-Andrei.local>
2026-01-21 20:32:26 +0200

    feat(ui): add visuals index export

---
+++ components/game/visuals/index.ts
@@ -0,0 +1,4 @@
+// components/game/visuals/index.ts
+export { PhospholipidMembrane } from './PhospholipidMembrane';
+export { TFProtein } from './TFProtein';
+export { ActivationArrow } from './ActivationArrow';
